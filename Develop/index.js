1d1a9e8d98510d9a404fca76905d5f273084ec6e
aq032 <anthonyq032@gmail.com>
2022-07-17 19:44:45 -0700

    Added questions and prompts for user

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,65 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Function that prompts user with questions
+const questions = () => {
+  const userQuestions = inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the title of your project?",
+      name: "title",
+      validate: userTitle => {
+        if (userTitle) {
+          return true;
+        } else {
+          console.log("Project title is required!")
+          return false;
+        }
+      }
+    },
+    {
+      type: "input",
+      message: "Write a detailed description of your project: ",
+      name: 'description'
+    },
+    {
+      type: "input",
+      message: "Write any installation instructions: ",
+      name: 'installation'
+    },
+    {
+      type: "input",
+      message: "Write any important usage information: ",
+      name: 'usage'
+    },
+    {
+      type: "input",
+      message: "What are your contribution guidelines?",
+      name: 'contribution'
+    },
+    {
+      type: "checkbox",
+      message: "What licenses will apply to your project? (Select all the apply)",
+      name: 'licenses',
+      choices: ["Apache", "GNU", "ISC", "MIT", "OBSD", "Public Domain (No license)"]
+    },
+    {
+      type: "input",
+      message: "What is your Github username?",
+      name: 'github',
+    },
+    {
+      type: "input",
+      message: "What is your email?",
+      name: 'email',
+    },
+  ])
+  return userQuestions
+}
+
+questions();
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+function writeToFile(fileName, data) {};
